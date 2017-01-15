154b3a991ff4f38ebcf11f031330ff303d76e47f
sht3ch <shtechgen@gmail.com>
2017-01-15 18:38:49 +0500

    Docker building with respect of new file structure

--- artm/docker/prepare_files.sh
+++ artm/docker/prepare_files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cp ../data/dict.filtered .
-cp ../data/big_model.artm.mtx .
-cp ../data/insception-classes.tsv .
+cp ../models/big_model.artm.mtx .
+cp ../resourses/insception-classes.tsv .
 
-jupyter nbconvert --to python ../notebooks/model_application.ipynb --output $(pwd)/model_application.py
-cp ../notebooks/utils.py .
+jupyter nbconvert --to python ../workspace/model_application.ipynb --output $(pwd)/model_application.py
+cp ../workspace/utils.py .

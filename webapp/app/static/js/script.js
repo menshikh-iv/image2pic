51e6fb212ef705556eb0ecf4b1e7138cff7d7564
ivan <menshikh.iv@gmail.com>
2017-01-29 12:56:14 +0500

    Add frontend part for voting & remove tags from search results

--- webapp/app/static/js/script.js
+++ webapp/app/static/js/script.js
@@ -19,3 +19,28 @@ $("#dataform").submit(function(e) {
     e.preventDefault();
 
 });
+
+function send_vote(self, vote){
+    var uvote = parseInt(vote);
+    var row = $(self).closest("tr");
+    var dist = parseFloat(row.find("td#dist").text());
+    var text = row.find("td#text").text();
+    var img_url = row.find("td>img#img_url").attr("src");
+
+    var query_text = $("table>tbody>tr>td#query_text").text();
+    var query_img = $("table>tbody>tr>td>img#query_img").attr("src");
+    var query_img_src = query_img ? query_img : "";
+
+    console.log(JSON.stringify({
+        "dist": dist,
+        "text": text,
+        "img_url": img_url,
+        "query_text": query_text,
+        "query_img": query_img_src,
+        "vote": uvote
+    }));
+
+    var td = $(self).closest("td");
+    td.find("button").remove();
+    (uvote == 1) ? td.attr("bgcolor", "#5cb85c") : td.attr("bgcolor", "#c9302c");
+}

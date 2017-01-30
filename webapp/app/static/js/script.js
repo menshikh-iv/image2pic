da029a949fcb47c6007cda1d5aacf4b51233a554
ivan <menshikh.iv@gmail.com>
2017-01-30 20:37:36 +0500

    hotfix

--- webapp/app/static/js/script.js
+++ webapp/app/static/js/script.js
@@ -6,8 +6,8 @@ function send_vote(self, vote){
     var img_url = row.find("td>img#img_url").attr("src");
 
     var query_text = $("table>tbody>tr>td#query_text").text();
-    var query_img = $("table>tbody>tr>td>img#query_img").attr("src").replace("data:image/png;base64,", "");
-    var query_img_src = query_img ? query_img : "";
+    var query_img = $("table>tbody>tr>td>img#query_img").attr("src");
+    var query_img_src = query_img ? query_img.replace("data:image/png;base64,", "") : "";
 
     var data = {
         dist: dist,

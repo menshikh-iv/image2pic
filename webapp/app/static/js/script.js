7167c0d50424b9941bf579f443f3399328f5f353
ivan <menshikh.iv@gmail.com>
2017-02-10 01:46:06 +0500

    Add spoiler[2]

--- webapp/app/static/js/script.js
+++ webapp/app/static/js/script.js
@@ -33,13 +33,3 @@ function send_vote(self, vote){
         }
     });
 }
-
-function spoiler() {
-    var s = $("#spoiler");
-    if (s.css("display") == "none") {
-        s.css("display", "");
-    }
-    else {
-        s.css("display", "none");
-    }
-}
